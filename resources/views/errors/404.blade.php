16c552dce718bf1b0d4fba89a0adefaa6880db55
Hanley Chan <hanleychan@gmail.com>
2016-06-29 12:28:28 -0700

    Added 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h1>404: Page Not Found</h1>
+    <p>Sorry, the page that you are looking for could not be found.</p>
+</div>
+@endsection
